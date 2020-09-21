29c434a7ecbb774e24f25a22bcd6cc5c15a1b0d9
favzz <ututufavour7@gmail.com>
2020-09-21 16:56:31 +0100

    added routes to employees.js

--- routes/employees.js
+++ routes/employees.js
@@ -1,12 +1,108 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+ const router = express.Router();
 
-// router.get('/', (req,res)=>{
-//     res.render('index')
-// })
-// router.get('/employee/new', (req,res)=>{
-//     res.render('new');
-// })
+// Routers
+//get routes starts here
+
+router.get('/', (req,res)=>{
+    Employee.find({})
+       .then(employees => {
+          res.render('index', {employees: employees})
+       })
+       .catch(err =>{
+          req.flash('error_msg', 'ERROR: '+ err)
+          res.redirect('/')
+       })
+   
+ })
+ router.get('/employee/new', (req,res)=>{
+    res.render('new')
+ })
+ router.get('/employee/search', (req,res)=>{
+    res.render('search', {employee:""})
+ })
+ router.get('/employee', (req,res)=>{
+    let searchQuery= {name:req.query.name}
+    Employee.findOne(searchQuery)
+ 
+       .then(employee =>{
+          res.render('search', {employee: employee})
+       })
+       .catch(err =>{
+          req.flash('error_msg', 'ERROR: '+ err)
+           res.redirect('/')
+       });
+ })
+ router.get('/edit/:id', (req,res) =>{
+    let searchQuery = {_id: req.params.id};
+    Employee.findOne(searchQuery)
+       .then(employee => {
+          res.render('edit', {employee:employee})
+       })
+       .catch(err =>{
+          req.flash('error_msg', 'ERROR: '+ err)
+          res.redirect('/')
+       })
+ })
+ //get routes ends here
+ 
+ 
+ //post routes starts here
+ router.post('/employee/new',(req,res)=>{
+    let newEmployee = {
+       name: req.body.name,
+       designation: req.body.designation,
+       salary: req.body.salary
+    };
+ 
+    Employee.create(newEmployee)
+          .then(employee =>{
+             req.flash('success_msg', 'employee data added to database successfully')
+             res.redirect('/')
+          })
+          .catch(err =>{
+             req.flash('error_msg', 'ERROR: '+ err)
+             res.redirect('/')
+          });
+ });
+ //post routes ends here
+ 
+ //put routes starts here
+ router.put('/edit/:id', (req,res) =>{
+    let searchQuery = {_id:req.params.id};
+    Employee.updateOne(searchQuery, {$set:{
+       name: req.body.name,
+       designation:req.body.designation,
+       salary: req.body.salary
+    }})
+       .then(employee =>{
+          req.flash('success_msg', 'employee data updated successfully')
+          res.redirect('/')
+       })
+       .catch(err =>{
+          req.flash('error_msg', 'ERROR: '+ err)
+          res.redirect('/')
+       })
+ })
+ 
+ //put routes ends here
+ 
+ //delete routes starts here
+    router.delete('/delete/:id', (req,res)=>{
+       let searchQuery = {_id:req.params.id};
+ 
+       Employee.deleteOne(searchQuery)
+          .then(employee =>{
+             req.flash('success_msg', 'employee deleted successfully')
+             res.redirect('/')
+          })
+          .catch(err =>{
+             req.flash('error_msg', 'ERROR: '+ err)
+             res.redirect('/')
+          })
+    });
+ 
+ //delete routes ends here
 
 
-// module.exports = router;
+ module.exports = router;
